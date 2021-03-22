66ebb406921db9b49c1e0ae6f4ed20e178354674
Tadeo Mocoroa <juan_tadeo25@hotmail.com>
2021-03-22 18:34:48 -0300

    Agregamos state

--- src/components/Formtarea.js
+++ src/components/Formtarea.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Formtarea() {
-  return (
-    <div className="container my-5 d-flex justify-content-center">
-      <form className="w-75">
-        <div className="d-flex mt-3">
-          <input
-            type="text"
-            placeholder="Ingrese una tarea"
-            className="form-control"
-          />
-          <button className="btn btn-outline-light">Agregar</button>
+//Aqui escribo codigo js
+const [tarea,setTarea] = useState('');
+const [listaTareas,setListaTareas] = useState([])
+
+// const guardarTarea = (e) => {
+//     console.log(e.target.value);
+//     setTarea(e.target.value);
+// }
+    return (
+//Aqui escribo codigo js
+        <div className="container my-5 d-flex justify-content-center">
+            <form className="w-75">
+                <div className="d-flex mt-3">
+                    <input
+                        type="text"
+                        placeholder="Ingrese una tarea"
+                        className="form-control"
+                        onChange={(e) => setTarea(e.target.value)}
+                    />
+                    <button className="btn btn-warning ml-2">
+                        <b>Agregar</b>
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 }
