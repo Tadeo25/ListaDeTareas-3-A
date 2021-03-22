6990092afc91123eb8dfa96fa156645b6ecb77f3
Tadeo Mocoroa <juan_tadeo25@hotmail.com>
2021-03-22 17:13:24 -0300

    Agregamos componente FormTarea

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Titulo from "./components/Titulo";
 import Subtitulo from "./components/Subtitulo";
 import Header from "./components/Header";
+import Formtarea from "./components/Formtarea";
 
 function App() {
   return (
     <>
-      <Header logo="https://rollingcodeschool.com/wp-content/uploads/2019/12/ingenia-logo-transparencia_Mesa-de-trabajo-1-copia-e1575648427572.png"/>
-      <Titulo emoji="📒"/>
+      <Header logo="https://rollingcodeschool.com/wp-content/uploads/2019/12/ingenia-logo-transparencia_Mesa-de-trabajo-1-copia-e1575648427572.png" />
+      <Titulo emoji="📒" />
       <Subtitulo comision="3A" />
+      <Formtarea/>
     </>
   );
 }
--- src/components/Formtarea.js
+++ src/components/Formtarea.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Formtarea() {
-    return (
-        <div>
-            
+  return (
+    <div className="container my-5 d-flex justify-content-center">
+      <form className="w-75">
+        <div className="d-flex mt-3">
+          <input
+            type="text"
+            placeholder="Ingrese una tarea"
+            className="form-control"
+          />
+          <button className="btn btn-outline-light">Agregar</button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
